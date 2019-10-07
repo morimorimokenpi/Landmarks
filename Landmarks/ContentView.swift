11769e0501d00792d632de341886a7f29e78cc8f
morimorimokenpi <aaa.ys40.y2@gmail.com>
2019-10-07 22:41:50 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 小林泰宣 on 2019/10/07.
+//  Copyright © 2019 morimorimokenpi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
